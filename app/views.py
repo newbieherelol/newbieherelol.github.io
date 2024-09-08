eb7fac5567b2f444a661bd68403ad36298e96eb3
newbieherelol <ptnk3636laptrinh@gmail.com>
2024-09-08 15:47:38 +0700

    Delete app directory

--- app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-   response = HttpResponse()
-   return response
-   return render(request, 'pages/home.html')
